2214edab3a506591cd148632bfe14295cb2fda10
Petr Ivanov <d8ml@yandex.ru>
2024-03-11 09:18:42 +0500

    Initial commit

---
+++ src/main/java/hh/forest_of_habits/ForestOfHabitsApplication.java
@@ -0,0 +1,13 @@
+package hh.forest_of_habits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestOfHabitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestOfHabitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hh/forest_of_habits/ForestOfHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.forest_of_habits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestOfHabitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
