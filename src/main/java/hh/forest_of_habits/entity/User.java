0c9d4b290efa29c1cf097e412a0b5ef595ca6be2
Petr Ivanov <d8ml@yandex.ru>
2024-04-19 05:53:53 +0500

    Добавил тесты UserService и AuthService
    Добавил тесты UserService и AuthService

--- src/main/java/hh/forest_of_habits/entity/User.java
+++ src/main/java/hh/forest_of_habits/entity/User.java
@@ -1,10 +1,16 @@
 package hh.forest_of_habits.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="users")
 public class User {
     @Id
