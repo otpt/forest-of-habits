5cdd6ab20a8f73b07ec9a4a5c632993bcd310a63
Игнатенко Ярослав Игоревич <ignatenkoyaroslav1@gmail.com>
2024-04-26 23:18:28 +0300

    Подготовил базовые настройки для интеграционных тестов

--- src/test/java/hh/forest_of_habits/ForestOfHabitsApplicationTests.java
+++ src/test/java/hh/forest_of_habits/ForestOfHabitsApplicationTests.java
@@ -1,15 +1,64 @@
 package hh.forest_of_habits;
 
+import hh.forest_of_habits.entity.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RootUriBuilderFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Disabled
-@SpringBootTest
+import java.net.URI;
+import java.util.Map;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(initializers = {ForestOfHabitsApplicationTests.Initializer.class})
+@ActiveProfiles("test")
+@Testcontainers
 class ForestOfHabitsApplicationTests {
 
-    @Test
-    void contextLoads() {
+    static WebTestClient webClient;
+
+    static final User testUser = User.builder()
+            .name("testuser1")
+            .password("123qwe")
+            .email("testemail@gmail.com")
+            .agreementConfirmation(true)
+            .build();
+
+    @BeforeAll
+    static void setUp() {
+        webClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:8888")
+                .build();
     }
 
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.2")
+            .withReuse(true)
+            .withDatabaseName("forest_of_habits");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "CONTAINER.USERNAME=" + postgreSQLContainer.getUsername(),
+                    "CONTAINER.PASSWORD=" + postgreSQLContainer.getPassword(),
+                    "CONTAINER.URL=" + postgreSQLContainer.getJdbcUrl()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 }
