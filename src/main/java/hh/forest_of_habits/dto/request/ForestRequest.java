6332d8a7707466a3c4582e936cd0b39b57e54ed7
nik2025311 <nik2025311@yandex.ru>
2024-05-07 13:23:14 +0300

    feat: добавить валидацию входных параметров и тел запросов

--- src/main/java/hh/forest_of_habits/dto/request/ForestRequest.java
+++ src/main/java/hh/forest_of_habits/dto/request/ForestRequest.java
@@ -1,5 +1,6 @@
 package hh.forest_of_habits.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ForestRequest {
+    @NotBlank(message = "Название леса не может быть пустым")
     private String name;
 }
--- src/main/java/hh/forest_of_habits/dto/request/TreeRequest.java
+++ src/main/java/hh/forest_of_habits/dto/request/TreeRequest.java
@@ -3,6 +3,8 @@ package hh.forest_of_habits.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import hh.forest_of_habits.enums.TreePeriod;
 import hh.forest_of_habits.enums.TreeType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class TreeRequest {
+    @NotBlank(message = "Название дерева не может быть пустым")
     private String name;
     private String description;
+    @NotBlank(message = "Тип дерева должен быть указан")
     private TreeType type;
     private TreePeriod period;
     private Integer limit;
     @JsonProperty("forest_id")
+    @Positive(message = "ID леса, к которому относится дерево, должен быть больше или равен 1")
     private Long forestId;
 }
