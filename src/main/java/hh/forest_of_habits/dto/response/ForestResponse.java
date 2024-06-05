87ad49dc486b20e7394fe8fc50709dee17196091
nik2025311 <nik2025311@yandex.ru>
2024-06-05 14:17:41 +0300

    fix: добавить в возвращаемый лес поле sharedId

--- src/main/java/hh/forest_of_habits/dto/response/ForestResponse.java
+++ src/main/java/hh/forest_of_habits/dto/response/ForestResponse.java
@@ -1,5 +1,6 @@
 package hh.forest_of_habits.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,14 +8,17 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ForestResponse {
     private Long id;
     private String name;
     private LocalDateTime createdAt;
     private List<TreeResponse> trees;
+    private UUID sharedId;
 }
